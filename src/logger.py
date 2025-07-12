8f947c7cc31529f7bd6ed71ce22813a933561c89
jann-nick <microyahoo@gmail.com>
2025-07-13 06:37:17 +1200

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,25 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%D_%Y_%H_%M_%S')}.log"
+
+""" 
+ :: OLD CODE ::
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE) """
+
+# Create logs directory path
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Create full path to log file
+logs_path = os.path.join(logs_dir, LOG_FILE)
+
+logging.basicConfig(
+    filename=logs_path,
+    format="[%(asctime)s ]%(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
